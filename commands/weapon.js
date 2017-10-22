f40cedbdc8196bb30bae3f9b5aa89c232bbb4f5f
Unknown <boris.strots@gmail.com>
2017-10-22 13:04:32 -0700

    Multiple Word Weapons
    Added support for weapons to be called without underscores and with the brand being in front of the weapon name.

--- commands/weapon.js
+++ commands/weapon.js
@@ -4,21 +4,30 @@ const weaponData = require("../splatoon2-data/main.json");
 let color = 0;
 
 exports.run = (client, message, args) => {
+
   // Create RichEmbed and set values.
-  var weapon = args[1]
-  if (weaponData[weapon]) {
-    // Create and set up embed.
-    let weaponStyle = new Discord.RichEmbed();
-    weaponStyle.setColor(colorPicker());
-    weaponStyle.setTitle(weapon);
-    weaponStyle.setDescription("sub weapon: " + weaponData[weapon].sub_key);
-    // Upload Embed.
-    message.channel.send(weaponStyle);
+  var weapon;
+  // This is pretty bad. Please suggest any changes!
+  if (weaponData[args[1] + "_" + args[2] + "_" + args[3]]) {
+    weapon = args[1] + "_" + args[2] + "_" + args[3];
+    sendWeaponEmbed(message, weapon);
+  } else if (weaponData[args[3] + "_" + args[1] + "_" + args[2]]) {
+    weapon = args[3] + "_" + args[1] + "_" + args[2];
+    sendWeaponEmbed(message, weapon);
+  } else if (weaponData[args[1] + "_" + args[2]]) {
+    weapon = args[1] + "_" + args[2];
+    sendWeaponEmbed(message, weapon);
+  } else if (weaponData[args[2] + "_" + args[1]]) {
+    weapon = args[2] + "_" + args[1];
+    sendWeaponEmbed(message, weapon);
+  } else if (weaponData[args[1]]) {
+    weapon = args[1];
+    sendWeaponEmbed(message, weapon);
   } else {
     message.channel.send("that is not a weapon my dude");
   }
 }
-
+// For alternating colors.
 function colorPicker() {
   if (color == 0) {
     color = 1;
@@ -27,4 +36,14 @@ function colorPicker() {
     color = 0;
     return "#F02D7D";
   }
+}
+
+function sendWeaponEmbed(message, weapon) {
+  // Create and set up embed.
+  let weaponStyle = new Discord.RichEmbed();
+  weaponStyle.setColor(colorPicker());
+  weaponStyle.setTitle(weapon);
+  weaponStyle.setDescription("sub weapon: " + weaponData[weapon].sub_key);
+  // Upload Embed.
+  message.channel.send(weaponStyle);
 }
