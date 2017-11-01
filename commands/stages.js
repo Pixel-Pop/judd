d42a98eb7f8f4bb7a540df3718e437bf3cf8760b
Unknown <boris.strots@gmail.com>
2017-10-31 18:00:07 -0700

    Turf, Ranked, League Stages
    User can now view current schedules of turf, ranked, and league by typing in corresponding mode as an argument. Shows both stages when showing pictures. More colors and slight design change.

--- commands/stages.js
+++ commands/stages.js
@@ -11,22 +11,53 @@ exports.run = (client, message, args) => {
     });
     res.on("end", () => {
       stageData = JSON.parse(body);
-      sendStageEmbed(message, stageData);
+      asdf(message, args, stageData);
     });
   }).on("error", function(e){
     console.log("Got an error: ", e);
   });
 }
 
-function sendStageEmbed(message, data) {
+function asdf(message, args, data) {
+  const a = {
+    "turf": [data.regular, "#19D719", "#215121", "battle-regular.01b5ef.png"],
+    "ranked": [data.gachi, "#F54910", "#FF470F", "battle-ranked.78832c.png"],
+    "league": [data.league, "#F02D7D", "#B02C5B", "battle-league.95f523.png"]
+  }
+  let mode = a[args[1]];
+  if (mode) {
+    sendModeStageEmbed(message, args, data, mode)
+  } else {
+    sendStagesEmbed(message, data);
+  }
+}
+
+function sendStagesEmbed(message, data) {
+  // TODO
+}
+
+function sendModeStageEmbed(message, args, data, mode) {
+  let color1 = mode[1];
+  let color2 = mode[2];
+  let modeImgUrl = mode[3];
+  mode = mode[0];
   // Create and set up embed.
   let stageStyle = new Discord.RichEmbed();
-  imgURL = data.league[0].stage_a.image.substring(14);
-  stageStyle.setColor("#F02D7D");
-  stageStyle.setTitle("League Battle Stages - " + data.league[0].rule.name);
-  stageStyle.setThumbnail("https://splatoon2.ink/assets/img/battle-league.95f523.png");
-  stageStyle.setDescription(data.league[0].stage_a.name);
-  stageStyle.setImage("https://splatoon2.ink/assets/img/splatnet/"+imgURL);
+  stageImgUrl = mode[0].stage_a.image.substring(14);
+  stageStyle.setColor(color1);
+  stageStyle.setAuthor(mode[0].game_mode.name+ " Stages - " + mode[0].rule.name, "https://splatoon2.ink/assets/img/" + modeImgUrl);
+//  stageStyle.setTitle("League Battle Stages - " + data.league[0].rule.name);
+  //stageStyle.setThumbnail("https://splatoon2.ink/assets/img/battle-league.95f523.png");
+  stageStyle.setDescription(mode[0].stage_a.name);
+  stageStyle.setImage("https://splatoon2.ink/assets/img/splatnet/"+stageImgUrl);
   message.channel.send(stageStyle);
-  message.channel.send(data.league[0].stage_a.name+data.league[1].stage_b.name);
+  // Create and send second embed.
+  stageImgUrl = mode[0].stage_b.image.substring(14);
+  stageStyle.setColor(color2);
+  stageStyle.setAuthor(mode[0].game_mode.name+ " Stages - " + mode[0].rule.name, "https://splatoon2.ink/assets/img/" + modeImgUrl);
+  //stageStyle.setTitle("League Battle Stages - " + data.league[0].rule.name);
+  //stageStyle.setThumbnail("https://splatoon2.ink/assets/img/battle-league.95f523.png");
+  stageStyle.setDescription(mode[0].stage_b.name);
+  stageStyle.setImage("https://splatoon2.ink/assets/img/splatnet/"+stageImgUrl);
+  message.channel.send(stageStyle);
 }
