8b2ef5fddcf6ae254977426bad1c5282edf48b8b
Unknown <boris.strots@gmail.com>
2017-11-13 22:09:21 -0800

    Stage Colors
    Refined colors sent with stage embeds. General better code maintanability. Added inkbrush nouveau.

--- commands/stages.js
+++ commands/stages.js
@@ -1,3 +1,4 @@
+// Import needed resources.
 const Discord = require("discord.js");
 const https = require("https");
 
@@ -6,57 +7,62 @@ exports.run = (client, message, args) => {
   var url = "https://splatoon2.ink/data/schedules.json";
   https.get(url, (res) => {
     let body = "";
+    // Each time a packet is received, add it to the data.
     res.on("data", (chunk) => {
       body += chunk;
     });
+    // When all packets have been received, run the main program.
     res.on("end", () => {
       stageData = JSON.parse(body);
-      asdf(message, args, stageData);
+      main(message, args, stageData);
     });
+
   }).on("error", function(e){
     console.log("Got an error: ", e);
   });
-}
+};
 
-function asdf(message, args, data) {
-  const a = {
-    "turf": [data.regular, "#8ec902", "#19D719", "battle-regular.01b5ef.png"],
-    "ranked": [data.gachi, "#f36c01", "#f54910", "battle-ranked.78832c.png"],
+function main(message, args, data) {
+  // Get the needed styles for each stage embed.
+  const stageStyles = {
+    "turf": [data.regular, "#8EC902", "#19D719", "battle-regular.01b5ef.png"],
+    "regular": [data.regular, "#8EC902", "#19D719", "battle-regular.01b5ef.png"],
+    "ranked": [data.gachi, "#F36C01", "#F54910", "battle-ranked.78832c.png"],
     "league": [data.league, "#F02D7D", "#B02C5B", "battle-league.95f523.png"]
-  }
-  let mode = a[args[1]];
-  if (mode) {
-    sendModeStageEmbed(message, args, data, mode)
+  };
+  let modeStyle = stageStyles[args[1]];
+  /* If the user requests a certain mode, send only that mode with greater
+     detail. Else, send the more general stage list.*/
+  if (modeStyle) {
+    sendModeStageEmbed(message, args, data, modeStyle);
   } else {
     sendStagesEmbed(message, data);
   }
 }
 
 function sendStagesEmbed(message, data) {
-  // TODO
 }
 
 function sendModeStageEmbed(message, args, data, mode) {
+  // Set the properties of the embed.
   let color1 = mode[1];
   let color2 = mode[2];
   let modeImgUrl = mode[3];
   mode = mode[0];
-  // Create and set up embed.
+
+  // Create and set up the properties of stage_a embed.
   let stageStyle = new Discord.RichEmbed();
   stageImgUrl = mode[0].stage_a.image.substring(14);
   stageStyle.setColor(color1);
   stageStyle.setAuthor(mode[0].game_mode.name+ " Stages - " + mode[0].rule.name, "https://splatoon2.ink/assets/img/" + modeImgUrl);
-//  stageStyle.setTitle("League Battle Stages - " + data.league[0].rule.name);
-  //stageStyle.setThumbnail("https://splatoon2.ink/assets/img/battle-league.95f523.png");
   stageStyle.setDescription(mode[0].stage_a.name);
   stageStyle.setImage("https://splatoon2.ink/assets/img/splatnet/"+stageImgUrl);
   message.channel.send(stageStyle);
-  // Create and send second embed.
+
+  // Create and send up the properties of stage_b embed.
   stageImgUrl = mode[0].stage_b.image.substring(14);
   stageStyle.setColor(color2);
   stageStyle.setAuthor(mode[0].game_mode.name+ " Stages - " + mode[0].rule.name, "https://splatoon2.ink/assets/img/" + modeImgUrl);
-  //stageStyle.setTitle("League Battle Stages - " + data.league[0].rule.name);
-  //stageStyle.setThumbnail("https://splatoon2.ink/assets/img/battle-league.95f523.png");
   stageStyle.setDescription(mode[0].stage_b.name);
   stageStyle.setImage("https://splatoon2.ink/assets/img/splatnet/"+stageImgUrl);
   message.channel.send(stageStyle);
